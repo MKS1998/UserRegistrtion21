9008fed529216cb711913c13263d8fefe9bf1230
MKS1998 <nksayyad2900@outlook.com>
2021-10-17 16:25:15 +0530

    Added : Valid First Name Starts with Cap Has Min 3 Char

--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -18,4 +18,17 @@ public class UserRegistrationTest {
 		boolean result = userRegistration.firstName("moh");
 		Assert.assertEquals(false, result);
 	}
+
+	@Test
+	public void givenLastName_WhenInFormat_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Sai");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
+		boolean result = userRegistration.lastName("sai");
+		Assert.assertEquals(false, result);
+
+	}
 }
